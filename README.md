92992bccea8459385b34878c49031a65dbab6776
Marcos Gregorio <marquinhogreg2013@gmail.com>
2023-09-05 08:14:27 -0300

    ok

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # mqtt-subscriber
+
+`sudo mosquitto -c ./mosquitto.conf`
---
+++ teste.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <mosquitto.h>
+using namespace std;
+
+int main() {
+  cout << "Oiiii\n";
+
+  cout << "eae";
+  return 0;
+
+}
