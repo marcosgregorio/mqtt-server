4152ced1301ed1106099ab9deb4cfc1b049b24a7
Marcos Gregorio <marquinhogreg2013@gmail.com>
2023-10-15 11:13:42 -0300

    ok

---
+++ ChatApp/a.java
@@ -0,0 +1,12 @@
+package ChatApp;
+import org.eclipse.paho.client.mqttv5.MqttClient;
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.eclipse.paho.client.mqttv3.MqttException;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
+ 
+class a {
+    public static void main(String[] args) {
+        System.out.println("Oi");
+    }
+}
