dfc3fd9ea0617e1dc486d6deee39a6644ae16eb8
Marcos Gregorio <marquinhogreg2013@gmail.com>
2023-11-20 19:35:30 -0300

    ok

--- chatapp/src/main/java/com/app/App.java
+++ chatapp/src/main/java/com/app/App.java
@@ -49,7 +49,6 @@ public class App {
         System.out.println("2 - Inscrever-se em um tópico");
         System.out.println("3 - Enviar mensagem para um tópico");
         System.out.println("4 - Sessões pendentes");
-        System.out.println("5 - Listar contatos");
     }
 
     public static void submitMessageOneToOne(Messenger messanger) {
